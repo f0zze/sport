4574e4875300e810564bdebca1b6f77ff24095e8
Aigars B <a.beinarovics@gmail.com>
2016-11-30 22:42:41 +0200

    create config files for dev and prod

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-'use strict';
 const Koa = require('koa');
+const config = require('config');
 const app = new Koa();
 const routes = require('./src/router');
 
 const port = process.env.PORT || 5000;
 app.use(routes.routes());
+app.use(async(ctx, next)=> {
+    ctx.body = `Sports App ${config.get('env')}`;
+    await next();
+});
 
 app.listen(port);
 console.log('listening on port ' + port);
